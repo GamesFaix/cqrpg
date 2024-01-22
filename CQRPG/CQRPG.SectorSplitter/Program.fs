6eda52011ebcf285eb5f230ade68a12203bce048
James Faix <jamesthefaix@gmail.com>
2024-01-22 13:12:45 -0800

    Added Argu for CLI

--- CQRPG/CQRPG.SectorSplitter/Program.fs
+++ CQRPG/CQRPG.SectorSplitter/Program.fs
@@ -1,2 +1,31 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿
+open System
+open Argu
+
+type CliArguments =
+    | Source of string
+    | Out_Dir of string
+    | Sector_Size of int
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Source _ -> "specify a source image."
+            | Out_Dir _ -> "specify an output directory."
+            | Sector_Size _ -> "specify a sector size, in pixels."
+
+let parser = ArgumentParser.Create<CliArguments>(programName = "sector-splitter.exe")
+
+[<EntryPoint>]
+let main (args: string array) =
+    try
+        let results = parser.Parse args
+
+        let all = results.GetAllResults()
+
+        printfn "%A" all
+    with
+    | :? ArguParseException as e -> 
+        printfn "%A" e
+
+    0
