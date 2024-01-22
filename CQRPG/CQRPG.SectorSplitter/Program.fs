17f1c55d5c706c1105ed13b85ff79f17412ea7ed
James Faix <jamesthefaix@gmail.com>
2024-01-22 14:29:06 -0800

    Implemented splitter

--- CQRPG/CQRPG.SectorSplitter/Program.fs
+++ CQRPG/CQRPG.SectorSplitter/Program.fs
@@ -1,29 +1,42 @@
-﻿
-open System
-open Argu
+﻿open Argu
 
 type CliArguments =
+    | Working_Dir of string
     | Source of string
-    | Out_Dir of string
     | Sector_Size of int
 
     interface IArgParserTemplate with
         member s.Usage =
             match s with
+            | Working_Dir _ -> "specify a working directory."
             | Source _ -> "specify a source image."
-            | Out_Dir _ -> "specify an output directory."
             | Sector_Size _ -> "specify a sector size, in pixels."
 
 let parser = ArgumentParser.Create<CliArguments>(programName = "sector-splitter.exe")
 
+let parse args = 
+    let results = parser.Parse args
+    let parsed = {|
+        WorkingDir = results.GetResult(Working_Dir)
+        Source = results.GetResult(Source)
+        SectorSize = results.GetResult(Sector_Size)
+    |}
+    printfn "%A" parsed
+    parsed
+
 [<EntryPoint>]
 let main (args: string array) =
     try
-        let results = parser.Parse args
+        let parsedArgs = parse args
 
-        let all = results.GetAllResults()
+        let result = Splitter.split parsedArgs.WorkingDir parsedArgs.Source parsedArgs.SectorSize
 
-        printfn "%A" all
+        match result with
+        | Ok () ->
+            printfn "Done"
+        | Error e ->
+            printfn "Failed: %A" e
+
     with
     | :? ArguParseException as e -> 
         printfn "%A" e
---
+++ CQRPG/CQRPG.SectorSplitter/Splitter.fs
@@ -0,0 +1,61 @@
+﻿module Splitter
+
+open System
+open System.Drawing
+open System.IO
+
+type SplitterResult<'data> = Result<'data, string>
+
+let private loadSourceImage (source: string) : Image SplitterResult =
+    if not <| File.Exists source then
+        Error $"Source file not found: {source}"
+    else
+        let img = Image.FromFile(source)
+        Ok img
+
+let private createOutDirIfMissing (outDir: string) : unit SplitterResult =
+    if Directory.Exists outDir then
+       Ok ()
+    else
+        try 
+            outDir |> Directory.CreateDirectory
+            |> ignore
+            |> Ok
+        with
+        | e -> Error e.Message
+
+let private renderSector (outDir: string) (sourceName: string) (sourceImage: Image) (x: int, y: int) : unit SplitterResult =
+
+    let format n = n.ToString().PadLeft(2, '0')
+    let filename = Path.Combine(outDir, $"{sourceName}-{format x}-{format y}.png")
+
+    printfn $"rendering {filename}"
+
+    Ok ()
+
+let private getCellCoordinates (sourceWidth: int, sourceHeight: int) (sectorSize: int) : (int * int) list =
+    let columns = (float sourceWidth / float sectorSize) |> Math.Ceiling |> int
+    let rows = (float sourceHeight / float sectorSize) |> Math.Ceiling |> int
+    
+    seq {
+        for x in [1..columns] do
+            for y in [1..rows] do
+                yield (x, y)
+    }
+    |> Seq.toList
+
+let split (workingDir: string) (source: string) (sectorSize: int) : unit SplitterResult =
+    Path.Combine(workingDir, source)
+    |> loadSourceImage
+    |> Result.bind (fun sourceImage ->
+        let sourceName = Path.GetFileNameWithoutExtension source 
+        let outDir = Path.Combine(workingDir, sourceName)
+
+        createOutDirIfMissing outDir
+        |> Result.bind (fun _ -> 
+            getCellCoordinates (sourceImage.Width, sourceImage.Height) sectorSize
+            |> List.fold 
+                (fun _ cell -> renderSector outDir sourceName sourceImage cell) 
+                (Ok ())
+        )
+    )
